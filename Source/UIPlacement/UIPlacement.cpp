a1b4f506a37fb1cadb7b38c9134be3daac7e7a66
M Anees <96178974+AneesGameDev@users.noreply.github.com>
2024-02-29 12:43:13 +0500

    Initial commit

---
+++ Source/UIPlacement/UIPlacement.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UIPlacement.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UIPlacement, "UIPlacement" );
---
+++ Source/UIPlacement/UIPlacementGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UIPlacementGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UIPLACEMENT_API AUIPlacementGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
